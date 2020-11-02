6356374795fbf69d0f8a6fdb1cdcbbf99a56f492
Adam <57396704+Sankti@users.noreply.github.com>
2020-11-02 22:42:33 +0100

    Updates

---
+++ precache-manifest.d22fb468537347749e46603ea50fbb46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b9710e341f7eade32fd906999a4e9d",
+    "url": "/Palatinus/index.html"
+  },
+  {
+    "revision": "6be99e81a5b36ead5bc8",
+    "url": "/Palatinus/static/css/main.49e8e6d9.chunk.css"
+  },
+  {
+    "revision": "8c704548f66ca0b4439b",
+    "url": "/Palatinus/static/js/2.11670a56.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Palatinus/static/js/2.11670a56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be99e81a5b36ead5bc8",
+    "url": "/Palatinus/static/js/main.58b59e32.chunk.js"
+  },
+  {
+    "revision": "85f3c0a8708feba92d08",
+    "url": "/Palatinus/static/js/runtime-main.ee8d9ced.js"
+  }
+]);
